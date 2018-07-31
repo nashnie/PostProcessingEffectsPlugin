63a3ee1c43d68adb0e18c3e5d19312d1dd8d2dc0
聂述龙 <627972381@qq.com>
2018-07-31 13:40:57 +0800

    Initial commit

---
+++ Assets/Scripts/PostProcessingEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingEffect : MonoBehaviour
+{
+    private RenderTextureFactory renderTextureFactory;
+
+    private void OnEnable()
+    {
+        renderTextureFactory = new RenderTextureFactory();
+    }
+
+    private void OnDisable()
+    {
+        if (renderTextureFactory != null)
+        {
+            renderTextureFactory.Dispose();
+            renderTextureFactory = null;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+    }
+}
---
+++ Assets/Scripts/RenderTextureFactory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Nash
+/// </summary>
+public class RenderTextureFactory
+{
+    private HashSet<RenderTexture> tempRTMap;
+
+    public RenderTextureFactory()
+    {
+        tempRTMap = new HashSet<RenderTexture>();
+    }
+
+    public RenderTexture Get(RenderTexture baseRT)
+    {
+        return Get(baseRT.width, baseRT.height, baseRT.depth, baseRT.format, baseRT.sRGB ? RenderTextureReadWrite.sRGB : RenderTextureReadWrite.Linear);
+    }
+
+    public RenderTexture Get(RenderTexture baseRT, int downSample)
+    {
+        return Get(baseRT.width >> downSample, baseRT.height >> downSample, baseRT.depth, baseRT.format, baseRT.sRGB ? RenderTextureReadWrite.sRGB : RenderTextureReadWrite.Linear);
+    }
+
+    public RenderTexture Get(int width, int height, int depth, RenderTextureFormat format, RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default, FilterMode filterMode = FilterMode.Bilinear, string name = "RenderTextureFactoryRT")
+    {
+        RenderTexture temporary = RenderTexture.GetTemporary(width, height, depth, format, readWrite);
+        temporary.filterMode = filterMode;
+        temporary.name = name;
+        tempRTMap.Add(temporary);
+        return temporary;
+    }
+
+    public void Realease(RenderTexture tempRT)
+    {
+        tempRTMap.Remove(tempRT);
+        RenderTexture.ReleaseTemporary(tempRT);
+    }
+
+    public void RealeaseAll()
+    {
+        var enumerator = tempRTMap.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            RenderTexture.ReleaseTemporary(enumerator.Current);
+        }
+        tempRTMap.Clear();
+    }
+
+    public void Dispose()
+    {
+        RealeaseAll();
+    }
+}
